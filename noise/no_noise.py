62b6fc9799f8b256180d30e40190727126c75f1f
TenKeyLess <blivme84@naver.com>
2024-07-04 13:45:03 +0900

    * no noise implemented

--- noise/no_noise.py
+++ noise/no_noise.py
@@ -2,5 +2,5 @@ from noise.noise_interface import NoiseInterface
 
 class NoNoise(NoiseInterface):
     def apply(self, signal):
-        raise NotImplementedError("NoNoise 메소드 구현 필요")
+        return signal
         
